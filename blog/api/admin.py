b167e1390177796515b6547b9621883c42a34707
Кокушин Александр <o000oo73@mail.ru>
2023-03-17 21:17:48 +0400

    Add ModelAdmin

--- blog/api/admin.py
+++ blog/api/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AuthorPost, Blog, Comment, Follow, Post, Tag
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title',
+                    'description',
+                    'created_at',
+                    'updated_at',
+                    'owner'
+                    )
+    search_fields = ('owner',)
+    list_filter = ('updated_at',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(AuthorPost)
+class AuthorPostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'blog')
+    search_fields = ('user',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'slug')
+    search_fields = ('name',)
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author',
+                    'title',
+                    'body',
+                    'created_at',
+                    'likes',
+                    'views',
+                    'tags'
+                    )
+    search_fields = ('author',)
+    list_filter = ('created_at',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'body', 'created_at',)
+    search_fields = ('author',)
+    list_filter = ('created_at',)
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'blog')
+    search_fields = ('user',)
